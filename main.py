500fd7b3503a699ebb049768f90c42d2ba1ba7e3
Christopher Jr Riley <chrisjr@cjrriley.com>
2023-11-07 02:16:00 -0500

    Add boilerplate code to test basic bot ability

--- main.py
+++ main.py
@@ -3,14 +3,37 @@ main.py: The entry point for the Cephalon Seren Discord bot.
 This module initializes the bot, loads the necessary cogs,
 and starts the event loop.
 """
+# import discord
+# from discord.ext import commands
+
+# def main():
+#     """
+#     Main function to set up and run the Discord bot.
+#     """
+
+
+# if __name__ == "__main__":
+#     main()
+
+# This example requires the 'message_content' intent.
+
 import discord
-from discord.ext import commands
 
-def main():
-    """
-    Main function to set up and run the Discord bot.
-    """
+intents = discord.Intents.default()
+intents.message_content = True
 
+client = discord.Client(intents=intents)
 
-if __name__ == "__main__":
-    main()
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run('your token here')
