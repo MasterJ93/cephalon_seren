1655ced94f23c3578ca0b5350862d04afc0fe958
Christopher Jr Riley <chrisjr@cjrriley.com>
2023-11-07 01:48:17 -0500

    Import discord.py

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+import discord
+from discord.ext import commands
 
+"""
+main.py: The entry point for the Cephalon Seren Discord bot.
+This module initializes the bot, loads the necessary cogs,
+and starts the event loop.
+"""
 
 def main():
-    pass
+    """
+    Main function to set up and run the Discord bot.
+    """
 
 
 if __name__ == "__main__":
