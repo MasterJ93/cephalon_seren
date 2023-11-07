f8aaeaccd99d00388c4737f9760e7755bc689a4e
Christopher Jr Riley <chrisjr@cjrriley.com>
2023-11-07 03:07:53 -0500

    Run bot and remove boilerplate code

--- main.py
+++ main.py
@@ -27,27 +27,8 @@ def main():
         except Exception:
             print(f'Failed to load extension: ({extension}).')
 
+    bot.run(settings['DISCORD_TOKEN'])
+
 
 if __name__ == "__main__":
     main()
-
-# import discord
-
-# intents = discord.Intents.default()
-# intents.message_content = True
-
-# client = discord.Client(intents=intents)
-
-# @client.event
-# async def on_ready():
-#     print(f'We have logged in as {client.user}')
-
-# @client.event
-# async def on_message(message):
-#     if message.author == client.user:
-#         return
-
-#     if message.content.startswith('$hello'):
-#         await message.channel.send('Hello!')
-
-# client.run('your token here')
