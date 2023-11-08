ece0cf4dae2f7978cde9d24355c7dbd1923f12f6
Christopher Jr Riley <chrisjr@cjrriley.com>
2023-11-08 00:14:41 -0500

    Edit metadata with email and docstrings

--- __init__.py
+++ __init__.py
@@ -1,3 +1,7 @@
+"""
+Adds metadata information into this file for the package.
+"""
 __version__ = '0.1.0'
 __author__ = 'Christopher Jr Riley'
 __license__ = 'MIT'
+__email__ = 'chrisjr@cjrriley.com'
