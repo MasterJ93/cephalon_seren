4274ebf2f7dde10453cced77b27f76be2559abe0
Christopher Jr Riley <chrisjr@cjrriley.com>
2023-11-08 23:02:32 -0500

    Add role IDs and add new ID category

--- config.py
+++ config.py
@@ -9,12 +9,13 @@ settings = {
     'DISCORD_TOKEN': "", # Use 'os.getenv('DISCORD_TOKEN')' in production.
     'COMMAND_PREFIX': '/',
     'ROLE_ID': {
-        'OPERATOR': "",
-        'DRIFTER': "",
-        'ALLIANCE': "",
-        'WARLORD': "",
-        'CLAN': "",
-        'ADMIN': "",
-        'MOD': ""
+        'OPERATOR': "1066899336212004887",
+        'DRIFTER': "1066899404688207892",
+        'ALLIANCE': "1066969325581381672",
+        'WARLORD': "1169338858375237732",
+        'CLAN': "1066969178461970494",
+        'ADMIN': "885275629497499694",
+        'CLAN_MOD': "1066898095364915230",
+        'ALLIANCE_MOD': "1066899219828457512"
     }
 }
