f695ec0a7e90073417cdab4bea03429d7158cb7e
Christopher Jr Riley <chrisjr@cjrriley.com>
2023-11-10 00:46:50 -0500

    Add messages

---
+++ utils/messages.py
@@ -0,0 +1,23 @@
+"""
+Lists the messages available for Cephalon Seren.
+"""
+
+beginner = {
+    'INTRO': """Hi, {username}! Welcome to Shinobi of the Lotus. I\'m 
+    Cephalon Seren, a moderator bot in this server. 
+    Thanks for joining in!\n\n
+    Tell me: are you here to join the Shinobi of the Lotus clan? 
+    Or maybe you want to join one of our clans in our alliance? 
+    Or maybe you\'re already a member of a clan in here?\n\n
+    Please select an option.""",
+    'DROPDOWN_OPTION_1': 'I\'m joining the Shinobi of the Lotus clan.',
+    'DROPDOWN_OPTION_2': 'I\'m joining a clan in this alliance.',
+    'DROPDOWN_OPTION_3': 'I\'m already a member of one of the clans.',
+    'OPTION_1_RESPONSE': """Thanks for letting me know! I\'ll let an
+    admin know and they\'ll be with you soon.""",
+    'OPTION_2_RESPONSE': """Great! In the server, you\'ll find the 
+    {billboards} channel. Look through the list and see which ones 
+    you may be interested in. If you\'d like, you can even 
+    create your own!""",
+    'OPTION_3_RESPONSE': 'Great! Enjoy your stay in the server.'
+}
