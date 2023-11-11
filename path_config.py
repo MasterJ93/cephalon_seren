a6d44fcd2e5e32afe69f004c352282b3348cb48e
Christopher Jr Riley <chrisjr@cjrriley.com>
2023-11-11 04:25:01 -0500

    Remove path config file

--- path_config.py
+++
@@ -1,8 +0,0 @@
-"""
- Appends the project root to the system path.
-"""
-import sys
-import os
-
-# Append parent directory to the system path
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
