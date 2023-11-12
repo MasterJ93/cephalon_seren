bdf173b1c82df56e264c83f5f412510051c82850
Christopher Jr Riley <chrisjr@cjrriley.com>
2023-11-11 20:42:00 -0500

    Implement callback for BeginnerViewDropdown

--- views/beginner_views.py
+++ views/beginner_views.py
@@ -14,6 +14,36 @@ class BeginnerViewDropdown(discord.ui.Select):
         ]
         super().__init__(placeholder='Choose an option...', options=options)
 
+    async def callback(self, interaction: discord.Interaction):
+        seren_message = await interaction.original_response()
+        mentioned_member = seren_message.raw_mentions[0]
+        response = interaction.response
+
+        # Checks if the member who interacted with the original message is
+        # the one Seren mentioned.
+        if interaction.user.id is not mentioned_member:
+            return
+
+        if self.values[0]:
+            await response.send_message(
+                content=beginner['OPTION_1_RESPONSE'],
+                ephemeral=True,
+                delete_after=60.0
+            )
+        elif self.values[1]:
+            await response.send_message(
+                beginner['OPTION_2_RESPONSE'],
+                ephemeral=True,
+                delete_after=60.0
+            )
+        elif self.values[2]:
+            await response.send_message(
+                beginner['OPTION_3_RESPONSE'],
+                ephemeral=True,
+                delete_after=60.0
+            )
+
+
 class BeginnerView(discord.ui.View):
     """Contains a view for the beginner message."""
     def __init__(self):
