c7772eace1e0e47e8a669de81c7574cda114c6e9
Christopher Jr Riley <chrisjr@cjrriley.com>
2023-11-11 20:37:46 -0500

    Cleanup import lines

--- views/beginner_views.py
+++ views/beginner_views.py
@@ -2,8 +2,6 @@
 Contains views related to members that just came into the server.
 """
 import discord
-
-# from utils import messages
 from utils.messages import beginner
 
 class BeginnerViewDropdown(discord.ui.Select):
