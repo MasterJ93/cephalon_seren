0237f1270d954abbcf7c112e7403c68c2431ef6c
Christopher Jr Riley <chrisjr@cjrriley.com>
2023-11-19 09:34:19 -0500

    Add modal for basic information of the clan

---
+++ cogs/billboard.py
@@ -0,0 +1,49 @@
+"""
+A list of commands related to managing ads in the
+alliance-billboard channel.
+"""
+
+import discord
+from discord import Embed, app_commands
+from discord.ext import commands
+from config import settings
+from views.billboard_views import BillboardClanModal
+
+class BillBoardCommands(commands.Cog):
+    """
+    BillboardCommands Cog for handling billboard ad management.
+    """
+    def __init__(self, bot):
+        self.bot = bot
+
+    @app_commands.command(name="billboard")
+    @app_commands.choices(choices=[
+            app_commands.Choice(
+                name="create", value="create"),
+            app_commands.Choice(
+                name='edit', value="edit")
+    ])
+    @app_commands.checks.has_any_role(
+        settings['ROLE_ID']['WARLORD'],
+        settings['ROLE_ID']['ADMIN'])
+    async def billboard_command(self,
+                                interaction: discord.Interaction,
+                                # attachment: discord.Attachment,
+                                choices: app_commands.Choice[str]):
+        """
+        Create or edit a billboard ad.
+        """
+        guild = interaction.guild
+        billboard_channel = guild.get_channel( #type: ignore
+                settings['CHANNEL_ID']['ALLIANCE_BILLBOARD'])
+
+        if choices.value == 'create':
+            # await billboard_channel.send( #type: ignore
+            #     content='',
+            #     embed=Embed()
+            # )
+            await interaction.response.send_modal(BillboardClanModal(
+                interaction.guild, interaction.user))
+        elif choices.value == 'edit':
+            # message = await billboard_channel.fetch_message()
+            pass
---
+++ views/billboard_views.py
@@ -0,0 +1,72 @@
+"""
+Contains views related to commands related to managing billboard ads.
+"""
+
+import discord
+
+class BillboardView(discord.ui.View):
+    """docstring for BillboardView."""
+    def __init__(self,
+                 guild,
+                 member):
+        super().__init__()
+        self.guild = guild
+        self.member = member
+
+
+class BillboardClanModal(discord.ui.Modal):
+    """The modal used to enter the details for the Billboard ad."""
+    def __init__(self, guild, member):
+        super().__init__(title='Billboard Ad')
+        self.guild = guild
+        self.member = member
+
+     # This is to allow `clan_invite` to access this variable.
+    options = [
+        discord.SelectOption(
+            label="We're not accepting clan invites right now."
+        ),
+        discord.SelectOption(
+            label="We're currently accepting invites."
+        ),
+        discord.SelectOption(
+            label="We currently can't add any new members."
+        )
+    ]
+
+    clan_name = discord.ui.TextInput(
+        label="Clan name (3-digits after \"#\")",
+        placeholder='Shinobi of the Lotus#141',
+        custom_id='clan_name',
+        min_length=5,
+        max_length=100,
+        style=discord.TextStyle.short,
+        required=True
+    )
+
+    clan_description = discord.ui.TextInput(
+        label="Write a description of your clan.",
+        placeholder="Shinobi of the Lotus is a clan that helps players be "\
+            "masters at movement and navigation.",
+        custom_id='clan_des',
+        max_length=1024,
+        style=discord.TextStyle.long,
+        required=False
+    )
+
+    clan_requirements = discord.ui.TextInput(
+        label="Set the requirements of your clan.",
+        placeholder="We require members to be 18 or older.",
+        custom_id='clan_req',
+        max_length=300,
+        style=discord.TextStyle.long,
+        required=False
+    )
+
+    # clan_invite = discord.ui.Select(
+    #     custom_id='clan_inv',
+    #     options=options
+    # )
+
+    async def on_submit(self, interaction: discord.Interaction, /):
+        """When the \"Submit\" button is selected."""
