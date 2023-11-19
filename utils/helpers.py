65e635e5c0b3605fe16beff6c78ccfcefe8ffc45
Christopher Jr Riley <chrisjr@cjrriley.com>
2023-11-19 00:15:18 -0500

    Add helper file for miscellaneous methods

---
+++ utils/helpers.py
@@ -0,0 +1,34 @@
+"""Helper classes and methods."""
+import json
+
+class JSONReader():
+    """Reads JSON for Cephalon Seren."""
+    def __init__(self, json_file):
+        super().__init__()
+        self.json_file = json_file
+        self.rules = self.read_json_file()
+
+    def read_json_file(self):
+        """Reads the JSON file."""
+        try:
+            with open(self.json_file, 'r', encoding='utf-8') as file:
+                return json.load(file)
+        except FileNotFoundError:
+            print('File not found.')
+            return {"rules": []}
+        except json.JSONDecodeError:
+            print("Error decoding JSON.")
+            return {"rules": []}
+        except IOError as e:
+            print(f"IO error occurred: {e}")
+            return {"rules": []}
+
+    def get_selected_rules(self, rule_numbers):
+        """Puts the selected rules into a list."""
+        selected_rules = []
+        for rule in rule_numbers:
+            rule_index = int(rule.split('.')[0]) - 1
+            if 0 <= rule_index < len(self.rules['rules']):
+                selected_rules.append(self.rules['rules'][rule_index])
+
+        return selected_rules
