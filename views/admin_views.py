87ecfc717feb50e9b59711eae6352df4cd2ece13
Christopher Jr Riley <chrisjr@cjrriley.com>
2023-11-18 19:40:07 -0500

    Sort in ascending order and remove values

--- views/admin_views.py
+++ views/admin_views.py
@@ -19,6 +19,8 @@ class WarnView(discord.ui.View):
 
         self.confirm=False
         self.values=[]
+        self.message = ('Here are the rules you\'ve selected:' +
+                        '')
 
         # rules_dropdown = RulesDropdown()
         # self.add_item(rules_dropdown)
@@ -27,29 +29,23 @@ class WarnView(discord.ui.View):
     # This is to allow `rule_section` to access this variable.
     options = [
             discord.SelectOption(
-                label='1. Adhere to Discord ToS',
-                value='0x0'
+                label='1. Adhere to Discord ToS'
             ),
             discord.SelectOption(
-                label='2. No insults or hate speech',
-                value='0x1'
+                label='2. No insults or hate speech'
             ),
             discord.SelectOption(
-                label='3. No political/religious talk',
-                value='0x2'
+                label='3. No political/religious talk'
             ),
             discord.SelectOption(
-                label='4. No spamming/advertising',
-                value='0x3'
+                label='4. No spamming/advertising'
             ),
             discord.SelectOption(
-                label='5. No one under 18 in adult-only channels',
-                value='0x4'
+                label='5. No one under 18 in adult-only channels'
 
             ),
             discord.SelectOption(
-                label='6. No spoilers outside of spoiler channels',
-                value='0x5'
+                label='6. No spoilers outside of spoiler channels'
             )
     ]
 
@@ -63,12 +59,25 @@ class WarnView(discord.ui.View):
                              select: discord.ui.Select):
         """The dropdown menu that shows the TL;DR versions of the rules."""
         print(select.values)
-        self.values = select.values
+
+        # Sort the items in ascending order.
+        sorted_values = sorted(select.values, key=self.sorting_key)
+        self.values = sorted_values
+
+        # Loop through the list to grab each rule.
+        rules = ''
+        for rule in sorted_values:
+            rules += f'> {rule}\n'
+
+        print(rules)
+
         # Look for the Send Message button, then enable it.
         # We're using `#type: ignore` to stop Pylance from complaining.
         send_button = [child for child in self.children
                        if child.custom_id=='send_message'][0] #type: ignore
         send_button.disabled = False #type: ignore
+
+        # Edit the message to refresh.
         await interaction.response.edit_message(view=self)
 
     @discord.ui.button(label='Send Message',
@@ -91,5 +100,12 @@ class WarnView(discord.ui.View):
                            button: discord.ui.Button):
         """When the \"Cancel\" button is selected."""
         self.confirm=False
-        await interaction.delete_original_response()
         self.stop()
+
+    def sorting_key(self, key):
+        """
+        Custom key method to extract the numberic part of the 
+        selection.
+        """
+        number = int(key.split(".")[0])
+        return number
