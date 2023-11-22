df316195b5650923776e6884edba1b6f2873c593
Christopher Jr Riley <chrisjr@cjrriley.com>
2023-11-22 12:20:04 -0500

    Remove sample dictionary

--- database/clan_ad_db.py
+++ database/clan_ad_db.py
@@ -3,12 +3,4 @@ A database for temporarily storing clan ad information.
 """
 
 clan_ads = {
-    1234567890: {
-        'NAME': "Shinobi of the Lotus#141",
-        'DESCRIPTION': 'Our goal is to help you master your parkour skills in Warframe.',
-        'REQUIREMENTS': 'Be 18 or older.',
-        'CLAN_EMBLEM_URL': 'https://attachments.discord.com',
-        'INVITE_STATUS': '0x0',
-        'MESSAGE_ID': 12345678901
-    }
 }
