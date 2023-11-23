2d63b3be6e917a6edb4cf674303dc04c3125a224
Christopher Jr Riley <chrisjr@cjrriley.com>
2023-11-23 09:21:35 -0500

    Make ClanAdManager asynchronous and refactor
    There's a lot of code that's messy and needs to be cleaned up. Also, a ton
    of print statements are being used for testing. They'll soon
    be either removed or replaced with logger statements, depending on
    importance.

--- utils/clan_ad_manager.py
+++ utils/clan_ad_manager.py
@@ -3,8 +3,12 @@ Manages the clan ads temporarily when using the "/billboard Create Ad"
 Slash Command.
 """
 
+import asyncio
 import json
 from enum import Enum
+import os
+from typing import Optional
+import aiofiles
 
 
 class ClanAdKey(Enum):
@@ -17,6 +21,7 @@ class ClanAdKey(Enum):
     CLAN_EMBLEM_URL = 'CLAN_EMBLEM_URL'
     INVITE_STATUS = 'INVITE_STATUS'
     MESSAGE_ID = 'MESSAGE_ID'
+
 
 class IDAlreadyExistsException(Exception):
     """
@@ -31,99 +36,132 @@ class IDNotFoundException(Exception):
 
 
 class ClanAdManager():
-    """Accesses the clan ads within the \"clan_ad_db.py\" file."""
-    def __init__(self, user_id):
-        self.user_id = user_id
-        self.json_file = '../database/clan_ads_db.json'
-        self.clan_ads = self._load_data()
+    """Accesses the clan ads within the 'clan_ad_db.py' file."""
 
-    def _load_data(self):
+    def __init__(self):
+        self.lock = asyncio.Lock()
+
+        # Gets the directory where `clan_ad_manager.py` is located
+        base_dir = os.path.dirname(os.path.dirname(__file__))
+        # Constructs the path to the JSON file relative to the base directory
+        self.json_file = os.path.join(
+            base_dir, 'database', 'clan_ads_db.json'
+        )
+        # self.json_file = '../database/clan_ads_db.json'
+        self.clan_ads = {}
+
+    async def load_ads(self):
+        """
+        Loads the JSON into memory.
+        """
+        print('Loading the data.')
+        self.clan_ads = await self._load_data()
+
+    async def _load_data(self):
         try:
-            with open(self.json_file, 'r', encoding='utf-8') as file:
-                data = json.load(file)
-                return data.get("clan_ads", {})
+            print('Trying this method...')
+            async with self.lock:
+                print('Safety lock on.')
+                async with aiofiles.open(
+                        self.json_file, 'r', encoding='utf-8') as file:
+                    data = await file.read()
+                    print(f'data: {data}')
+                    return json.loads(data).get("clan_ads", {})
         except FileNotFoundError:
+            print("Can't find the data. Returning an empty dictionary.")
             return {}
 
-    def _save_data(self):
-        with open(self.json_file, 'w', encoding='utf-8') as file:
-            data = {"clan_ads": self.clan_ads}
-            json.dump(data, file, indent=4)
+    async def _save_data(self):
+        async with self.lock:
+            async with aiofiles.open(
+                    self.json_file, 'w', encoding='utf-8') as file:
+                data = {"clan_ads": self.clan_ads}
+                await file.write(json.dumps(data, indent=4))
 
-    def _get_user_id(self, user_id=None):
-        print(f'user_id is {user_id}. self.user_id is {self.user_id}.')
-        return user_id if user_id is not None else self.user_id
-
-    def create(self, user_id=None, name="", description="", requirements="",
-               clan_emblem_url="", invite_status="", message_id=0):
+    async def create(self, user_id, name="", description="", requirements="",
+                     clan_emblem_url="", invite_status="", message_id=0):
         """
         Creates a dictionary entry for the clan ad.
         """
-        user_id = self._get_user_id(user_id)
+        async with self.lock:
+            if user_id in self.clan_ads:
+                raise IDAlreadyExistsException(
+                    f"ID ({user_id}) already exists.")
 
-        if user_id in self.clan_ads:
-            raise IDAlreadyExistsException(f"ID ({user_id}) already exists.")
+            self.clan_ads[user_id] = {
+                'NAME': name,
+                'DESCRIPTION': description,
+                'REQUIREMENTS': requirements,
+                'CLAN_EMBLEM_URL': clan_emblem_url,
+                'INVITE_STATUS': invite_status,
+                'MESSAGE_ID': message_id
+            }
 
-        self.clan_ads[user_id] = {
-            'NAME': name,
-            'DESCRIPTION': description,
-            'REQUIREMENTS': requirements,
-            'CLAN_EMBLEM_URL': clan_emblem_url,
-            'INVITE_STATUS': invite_status,
-            'MESSAGE_ID': message_id
-        }
-        print(f'{user_id} has been created:\n{self.clan_ads[user_id]}')
-        self._save_data()
+        await self._save_data()
         return self.clan_ads[user_id]
 
+    # async def find(self, user_id: int):
+    #     """
+    #     Finds the dictionary entry for the clan ad.
+    #     """
+    #     user_id_str = str(user_id)
+    #     async with self.lock:
+    #         print('Locking .find()')
+    #         if user_id_str not in self.clan_ads:
+    #             print("Couldn't find ID in .find()")
+    #             raise IDNotFoundException(
+    #                 f"Couldn't find ID ({user_id_str}).")
 
-    def find(self, user_id: int):
+    #     return self.clan_ads[user_id_str]
+
+    async def read(self, user_id: str, key: Optional[ClanAdKey] = None):
         """
-        Finds the dictionary entry for the clan ad.
+        Reads the dictionary for the clan ad. If key is provided,
+        returns the specific value. Otherwise, returns the entire
+        clan ad entry.
         """
-        if user_id not in self.clan_ads:
-            raise IDNotFoundException(f"Couldn't find ID ({user_id}).")
+        async with self.lock:
+            print('Locking .read()')
+            print(f'self.clan_ads: {self.clan_ads}')
 
-        return self.clan_ads[user_id]
+            if user_id not in self.clan_ads:
+                print("Couldn't find ID in .read()")
+                raise IDNotFoundException(f"Couldn't find ID ({user_id}).")
 
-    def read(self, key: ClanAdKey, user_id=None):
-        """
-        Reads the dictionary for the clan ad.
-        """
-        user_id = self._get_user_id(user_id)
+            if key is not None:
+                return self.clan_ads[user_id].get(key.value)
+            else:
+                return self.clan_ads[user_id]
 
-        if user_id not in self.clan_ads:
-            raise IDNotFoundException(f"Couldn't find ID ({user_id}).")
-
-        return self.clan_ads[user_id].get(key.value)
-
-
-    def update(self, user_id=None, **kwargs):
+    async def update(self, user_id, **kwargs):
         """
         Updates a value for a key in the clan ad's dictionary.
         """
-        user_id = self._get_user_id(user_id)
+        _user_id = str(user_id)
+        async with self.lock:
+            if _user_id not in self.clan_ads:
+                print('Raising error in update().')
+                raise IDNotFoundException(f"Couldn't find ID ({_user_id}).")
 
-        if user_id not in self.clan_ads:
-            raise IDNotFoundException(f"Couldn't find ID ({user_id}).")
+            valid_keys = {key.value for key in ClanAdKey}
+            print(f"value_keys: {valid_keys}")
+            for key, value in kwargs.items():
+                print(f"Key-Value pair: ({key}), ({value})")
+                if key.upper() in valid_keys and value is not None:
+                    self.clan_ads[_user_id][key.upper()] = value
+                elif key.upper() not in valid_keys:
+                    raise ValueError(f"Key ({key}) is invalid.")
 
-        valid_keys = {key.value for key in ClanAdKey}
-        for key, value in kwargs.items():
-            if key.upper() in valid_keys and value is not None:
-                self.clan_ads[user_id][key.upper()] = value
-            elif key.upper() not in valid_keys:
-                raise ValueError(f"Key ({key}) is invalid.")
+        await self._save_data()
 
-        self._save_data()
-
-    def delete(self, user_id=None):
+    async def delete(self, user_id):
         """
         Deletes the entire clan ad's dictionary.
         """
-        user_id = self._get_user_id(user_id)
 
-        if user_id not in self.clan_ads:
-            raise IDNotFoundException(f"Couldn't find ID ({user_id}).")
+        async with self.lock:
+            if user_id not in self.clan_ads:
+                raise IDNotFoundException(f"Couldn't find ID ({user_id}).")
 
-        del self.clan_ads[user_id]
-        self._save_data()
+            del self.clan_ads[user_id]
+        await self._save_data()
