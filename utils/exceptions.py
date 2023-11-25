ea070e3838719b67d9c4bd61b26dadf2257001d8
Christopher Jr Riley <chrisjr@cjrriley.com>
2023-11-25 04:43:05 -0500

    Create exceptions.py
    We're going to encapsulate all of the custom exceptions into one list
    (instead of putting them into different files).

---
+++ utils/exceptions.py
@@ -0,0 +1,8 @@
+"""
+A list of custom exceptions for this bot to use.
+"""
+
+class InvalidFileType(Exception):
+    """
+    A custom Exception where an unsupported image format is detected.
+    """
