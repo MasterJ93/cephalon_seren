a565486707c6deba8793fe60ef73b9ba1f6a86d0
Christopher Jr Riley <chrisjr@cjrriley.com>
2023-11-26 19:42:18 -0500

    Refactor settings with environment variables
    Updated the configuration settings module to dynamically load
    Discord tokens, role IDs, and channel IDs from environment variables.

--- config.py
+++ config.py
@@ -3,19 +3,27 @@ This module contains configuration settings for Cephalon Seren, such as
 Discord tokens, database settings, and other constants required
 for operation.
 """
-# import os
+import os
 
 settings = {
-    'DISCORD_TOKEN': "", # Use 'os.getenv('DISCORD_TOKEN')' in production.
-    'COMMAND_PREFIX': '/',
+    'DISCORD_TOKEN': os.getenv('DISCORD_TOKEN'),
+    'COMMAND_PREFIX': os.getenv('COMMAND_PREFIX'),
     'ROLE_ID': {
-        'OPERATOR': "1066899336212004887",
-        'DRIFTER': "1066899404688207892",
-        'ALLIANCE': "1066969325581381672",
-        'WARLORD': "1169338858375237732",
-        'CLAN': "1066969178461970494",
-        'ADMIN': "885275629497499694",
-        'CLAN_MOD': "1066898095364915230",
-        'ALLIANCE_MOD': "1066899219828457512"
+        'RULES': int(os.getenv('RULES', '0')),
+        'OPERATOR': int(os.getenv('OPERATOR', '0')),
+        'DRIFTER': int(os.getenv('DRIFTER', '0')),
+        'ALLIANCE': int(os.getenv('ALLIANCE', '0')),
+        'WARLORD': int(os.getenv('WARLORD', '0')),
+        'CLAN': int(os.getenv('CLAN', '0')),
+        'ADMIN': int(os.getenv('ADMIN', '0')),
+        'CLAN_MOD': int(os.getenv('CLAN_MOD', '0')),
+        'ALLIANCE_MOD': int(os.getenv('ALLIANCE_MOD', '0'))
+    },
+    'CHANNEL_ID': {
+        'ALLIANCE_GENERAL': int(os.getenv('ALLIANCE_GENERAL', '0')),
+        'ADMIN_CHANNEL': int(os.getenv('ADMIN_CHANNEL', '0')),
+        'CLAN_GENERAL': int(os.getenv('CLAN_GENERAL', '0')),
+        'REPORTS_APPEALS': int(os.getenv('REPORTS_APPEALS', '0')),
+        'ALLIANCE_BILLBOARD': int(os.getenv('ALLIANCE_BILLBOARD', '0'))
     }
 }
