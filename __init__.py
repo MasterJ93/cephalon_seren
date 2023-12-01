aed8fa800dd0abcc445ebe1ca9f702575fce3365
Christopher Jr Riley <chrisjr@cjrriley.com>
2023-11-30 19:32:56 -0500

    Update to version 0.3.3

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 """
 Adds metadata information into this file for the package.
 """
-__version__ = '0.3.2'
+__version__ = '0.3.3'
 __author__ = 'Christopher Jr Riley'
 __license__ = 'MIT'
 __email__ = 'chrisjr@cjrriley.com'
